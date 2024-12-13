cb012dda40abf56955b9e02f1ac0e11f2f868b55
jose <jose@DESKTOP-IILTP2S>
2024-12-13 16:54:38 -0300

    nicolas vai arrumar

--- vender/views.py
+++ vender/views.py
@@ -6,64 +6,37 @@ from django.http import *
 import stripe
 from django.views.decorators.csrf import csrf_exempt
 
-stripe.api_key = settings.STRIPE_SECRET_KEY
 
 def create_checkout_session(request, id):
     if request.method == "POST":
-        produto = Produto.objects.get(id = id)
-        YOUR_DOMAIN = "http://127.0.0.1:8000"
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        produto = Produto.objects.get(id=id)
         checkout_session = stripe.checkout.Session.create(
             line_items=[
-            {
-                'price_data': {
-                'currency': 'BRL',
-                'unit_amount': int(produto.valor),
-                    'product_data': {
-                        'name': produto.nome
-                    }
-
-                },
-                'quantity': 1,
-            },
-        ],
-            payment_method_types=[
-                'card',
-                'boleto',
+                {
+                    "price_data": {
+                        "currency": "BRL",
+                        "unit_amount": int(produto.preco * 100),
+                        "product_data": {"name": produto.nome},
+                    },
+                    "quantity": 1,
+                }
             ],
+            payment_method_types=["card", "boleto"],
             metadata={
-                'id_produto': produto.id,
+                "id_produto": produto.id,
             },
-            mode='payment',
-            success_url=YOUR_DOMAIN + '/sucesso',
-            cancel_url=YOUR_DOMAIN + '/erro',
-            )
-        return redirect(create_checkout_session.url)
+            mode="payment",
+            success_url=request.build_absolute_uri(reverse("success")),
+            cancel_url=request.build_absolute_uri(reverse("cancel")),
+        )
+        return redirect(checkout_session.url, code=303)
     else:
         return redirect("home")
-
 def cancel(request):
     return render(request, "produto.html")
 
 
-@csrf_exempt
-def stripe_webnook(request):
-    payload = request.body
-    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-    event = None
-    endpoint_secret = settings.STRIPE_WEBNOOK_SECRET
-
-    try:
-        event = stripe.Webhook.construct_event(
-        payload, sig_header, endpoint_secret)
-    except ValueError as e:
-        # Invalid payload
-        return HttpResponse(status=400)
-    except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
-        return HttpResponse(status=400)
-    print(payload)
-
-    return HttpResponse(status=200)
 
 def produto(request, slug):
     produto = get_object_or_404(Produto, slug=slug)
